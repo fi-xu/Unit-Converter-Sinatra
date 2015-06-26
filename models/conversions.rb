2854a612f3f883ea448ba5f45983941671da818d
fi-xu <fiona.fx.xu@gmail.com>
2015-06-26 17:42:03 +0000

    pretty result page

--- models/conversions.rb
+++ models/conversions.rb
@@ -1,19 +1,19 @@
 def convert_sugar(grams)
   cups= grams.to_f/225
-  return cups
+  return cups.to_s + " cups."
 end
 
 def convert_flour(grams)
   cups=grams.to_f/125
-  return cups
+  return cups.to_s + " cups."
 end
 
 def convert_butter(grams)
   cups=grams.to_f/226
-  return cups
+  return cups.to_s + " cups."
 end
 
 def convert_powdered_sugar(grams)
   cups=grams.to_f/160
-  return cups
+  return cups.to_s + " cups."
 end
