32d479baec95b38b95eab2797b3ea07b62600ed7
fi-xu <fiona.fx.xu@gmail.com>
2015-06-26 15:20:50 +0000

    secondcommit

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,26 @@
 require 'bundler'
 Bundler.require
-
+require_relative "models/conversions.rb"
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  post '/' do
+    ingredient = params["ingredient"]
+    grams= params["grams"]
+    if ingredient == "sugar" 
+      @result=convert_sugar(grams)
+    elsif ingredient == "flour" || ingredient== "Flour"
+      @result=convert_flour(grams)
+    elsif ingredient== "butter"
+      @result= convert_butter(grams)
+    elsif ingredient=="powdered sugar" || ingredient=="confectioners sugar"
+      @result= convert_powdered_sugar(grams)
+    else @result="Sorry, that ingredient is not recognized. Please try again."
+    end
+    erb :result
+  end
 
 end
+
---
+++ models/conversions.rb
@@ -0,0 +1,19 @@
+def convert_sugar(grams)
+  cups= grams.to_f/225
+  return cups
+end
+
+def convert_flour(grams)
+  cups=grams.to_f/125
+  return cups
+end
+
+def convert_butter(grams)
+  cups=grams.to_f/226
+  return cups
+end
+
+def convert_powdered_sugar(grams)
+  cups=grams.to_f/160
+  return cups
+end
